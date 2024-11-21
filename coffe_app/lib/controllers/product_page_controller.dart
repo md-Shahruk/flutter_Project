1c936b8d6fab9c46ebe6d81bb96ae2f03e1c7696
Md. Shahruk <104712475+md-Shahruk@users.noreply.github.com>
2024-11-21 12:14:48 +0600

    Delete coffe_app directory

--- coffe_app/lib/controllers/product_page_controller.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:get/get.dart';
-
-class ProductPageController extends GetxController{
-  var sizeIndex=0.obs;
-  void updateSizeIndex(int index){
-    sizeIndex.value=index;
-  }
-}
--- coffe_app/lib/utils/color.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color brown = Color(0xFFC67C4E);       // A warm brown color
-  static const Color peach = Color(0xFFEED6C8);       // A soft peach color
-  static const Color darkGray = Color(0xFF313131);    // A dark gray color
-  static const Color lightGray = Color(0xFFE3E3E3);   // A very light gray color
-  static const Color offWhite = Color(0xFFF9F2ED);    // A cream/off-white color
-}
